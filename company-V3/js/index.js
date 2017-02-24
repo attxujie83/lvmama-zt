82097dd6e7aad0df44d827f104461160b40c7e55
xujie <54390704@qq.com>
2017-02-24 16:49:17 +0800

    1
    1

--- company-V3/js/index.js
+++ company-V3/js/index.js
@@ -96,14 +96,10 @@
 
         var $sliderDiv = _options.sliderDiv;
         var $picBox = _options.picBox;
-        var $bgBox = _options.bgBox;
         var $tabBox = _options.tabBox;
 
         var $picList = $picBox.find("li");
         var picAmount = $picList.length;
-		
-        var $bgList = $bgBox.find("li");
-        var bgAmount = $bgList.length;
 
         var $tabList = $tabBox.find("li");
         $tabList.eq(0).addClass("on");
@@ -118,7 +114,7 @@
             setInterval(function() {
                 if(isAutoRun){
 					var thisIndex= $tabBox.find("li.on").index();
-					if( thisIndex == picAmount-1 ){
+					if( thisIndex == picAmount-1){
 						showPic(0);
                     } else {
 						showPic(thisIndex + 1);
@@ -136,8 +132,6 @@
         	$tabList.removeClass("on").eq(index).addClass("on");
             $picList.stop(true,true).eq(PRE_INDEX).css("z-index","0").animate({opacity:"0"}, _options.animateSpeed);
             $picList.eq(index).css("z-index","1").animate({opacity:"1"}, _options.animateSpeed);
-            $bgList.stop(true,true).eq(PRE_INDEX).css("z-index","0").animate({opacity:"0"}, _options.animateSpeed);
-            $bgList.eq(index).css("z-index","1").animate({opacity:"1"}, _options.animateSpeed);
             PRE_INDEX = index;
         }
     }
@@ -148,9 +142,73 @@ $(function(){
     $.slider({
     	sliderDiv: $(".slider"),
         picBox: $(".slider-img-box"), 
-        bgBox: $(".slider-bg-img"), 
         tabBox: $(".slider-tab-box")
     });
 });
 
+/* jquery.slider.js START */
+
+(function($){
+    var defaults = {
+        animateSpeed: 800,
+        isAutoRun : true,
+        autoSpeed: 5000
+    };
+    //$picBox, $tabBox
+    $.slider = function(options){
+        var _options = $.extend(defaults, options);
+
+        var PRE_INDEX = 0;
+
+        var $sliderDiv = _options.sliderDiv;
+        var $picBox = _options.picBox;
+        var $tabBox = _options.tabBox;
+
+        var $picList = $picBox.find("li");
+        var picAmount = $picList.length;
+
+        var $tabList = $tabBox.find("li");
+        $tabList.eq(0).addClass("on");
+
+		$tabList.hover(function(){
+			showPic($(this).index());
+		},function(){});
+
+		//autoRun
+        if(_options.isAutoRun){
+        	var isAutoRun = _options.isAutoRun;
+            setInterval(function() {
+                if(isAutoRun){
+					var thisIndex= $tabBox.find("li.on").index();
+					if( thisIndex == picAmount-1){
+						showPic(0);
+                    } else {
+						showPic(thisIndex + 1);
+                    }
+                }
+            }, _options.autoSpeed);
+            $sliderDiv.hover(function() {
+                isAutoRun = false;
+            }, function() {
+                isAutoRun = true;
+            });
+        }
+
+        function showPic(index) {
+        	$tabList.removeClass("on").eq(index).addClass("on");
+            $picList.stop(true,true).eq(PRE_INDEX).css("z-index","0").animate({opacity:"0"}, _options.animateSpeed);
+            $picList.eq(index).css("z-index","1").animate({opacity:"1"}, _options.animateSpeed);
+            PRE_INDEX = index;
+        }
+    }
+
+})(jQuery);
+
+$(function(){
+    $.slider({
+    	sliderDiv: $(".slider-bg"),
+        picBox: $(".slider-bg-img"), 
+        tabBox: $(".slider-bg-tab")
+    });
+});
 
